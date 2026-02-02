504254cdbd5a25acd043573e37d1b5cdd6de841f
vikram52151 <vk8116771@gmail.com>
2026-02-02 22:18:14 +0530

    UI Version 2 completed

--- kosUI/src/app/app.config.ts
+++ kosUI/src/app/app.config.ts
@@ -1,17 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { LayoutModule } from './layout/layout.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-
     provideHttpClient(withFetch()),
 
-    // 👇 VERY IMPORTANT FOR ANGULAR MATERIAL
-    provideAnimations()
+    // Angular Material / Animations
+    provideAnimations(),
+    provideAnimationsAsync(),
+
+    // Layout module (router-outlet fix)
+    importProvidersFrom(LayoutModule)
   ]
 };
