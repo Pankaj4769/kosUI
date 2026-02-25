08c04087af5123886d1e673c0f29c901f4d6b998
Pankaj4769 <pankajraj4769@gmail.com>
2026-02-26 00:47:15 +0530

    auth guard changes

--- kosUI/src/app/core/guards/auth.guard.ts
+++ kosUI/src/app/core/guards/auth.guard.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
@@ -9,7 +10,7 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    if (this.authService.isLoggedIn) {
+    if (this.authService.isLoggedIn && (this.authService.currentUser?.subscriptionPlan != null || this.authService.currentUser?.subscriptionPlan != undefined )) {
       return true;
     }
     this.router.navigate(['/login']);
